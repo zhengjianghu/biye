462289a3ea59802564648c6c253a92cb3e10f788
JiangHu <991951038@qq.com>
2018-04-13 20:35:00 +0800

    再次删除

--- diploma project/毕设_学习汇总/ZAP/软件/ESP8266_RTOS_SDK-1.5/app/zaphost/insona_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INSONA_UTIL__
-#define __INSONA_UTIL__
-#include "app/framework/include/af.h"
-#include "app/framework/include/af-types.h"
-extern int32s string2SignedInt(char* data);
-extern int32u string2UnsignedInt(char* data);
-extern void sendDataParse(int8u* dstp,char* srdp);
-#endif
